8b8822b4309b53a539793aa86f11e7a35f318a7b
Filipi Gustavo Boltão <filipigustavoboltao@MacBook-Air-de-Filipi.local>
2020-10-13 11:59:23 -0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'hello'
+import 'hello/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
